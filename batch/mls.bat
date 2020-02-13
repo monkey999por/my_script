f98afdf1e7dafd0293d5280f19c110621b8456fe
monkey999por <yururi1122@gmail.com>
2020-02-14 00:00:19 +0900

    mls 途中

--- batch/mls.bat
+++ batch/mls.bat
@@ -1,4 +1,26 @@
 @echo off
 setlocal
 
+rem $mls %command_name% &command_option...&
+rem args
+rem command
+rem options...(for %command%)
+
+
 rem 引数チェック
+if "%1"=="" (
+ echo コマンド名を指定してください
+ exit /b 1
+)
+
+
+set "command=%1"
+
+
+
+
+
+
+
+
+
