f8e06fc0cacb3553eac3132f490a5874eac70b8f
monkey999por <yururi1122@gmail.com>
2020-02-14 01:18:15 +0900

    temp

---
+++ batch/help/hello_mls.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+
+echo ==Hello My Local Script=============Hello My Local Script=============Hello
+echo.
+echo ===============Hello My Local Script=========Hello My Local Script=========
+echo.
+echo　　　　　　　,.　-――‐-　._
+echo　　　　 ,.　'´ /⌒ヽ　/⌒ヽ ｀ヽ
+@echo　　　 /　 　 !　　ｏ l_!ｏ　　 !　　 ヽ
+echo..　　/　 ,. ‐へ＿,ノ-ヽ＿_ノ‐､　　ﾍ
+echo　　,'　/ ｀`'''ｰ-　 ﾞ､_,ﾉ　 -‐''" ヽ　 ',
+echo　　!　^l　￣￣_　　　^|　　 _￣￣　^l　 ^|   Hello My Local Script
+echo　　^l　^l　‐弋´　　　｜　　 ｀~ﾌｰ　!　^l
+echo　　', ',　 　 ＞-､_　^|　_,. -'"　　 / /
+echo　　　VV　 {　　 ） ￣　　　　　 / /
+echo　　　 ヽ／ ｀ー'＿＿＿＿＿,/／
+echo　　　 /　　　 /--/⌒ヽ----┘、
+echo　　　 ^l 　 　 /　　ﾋΤﾌ　　ヽ　　＼
+echo.
+echo ==Hello My Local Script=============Hello My Local Script=============Hello
+echo.
+echo ===============Hello My Local Script=========Hello My Local Script=========
+echo.
--- batch/mls.bat
+++ batch/mls.bat
@@ -1,10 +1,17 @@
 @echo off
 setlocal
 
+pushd "%~dp0"
+
 rem $mls %command_name% &command_option...&
 rem args
 rem command
 rem options...(for %command%)
+
+rem スタートページ
+.\help\hello_mls.bat
+
+
 
 
 rem 引数チェック
@@ -13,14 +20,13 @@ if "%1"=="" (
  exit /b 1
 )
 
-
 set "command=%1"
 
 
 
 
 
-
+popd
 
 
 
