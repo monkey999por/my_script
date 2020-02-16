cce386ad61ea38ba79573a6fc84176207fe74b75
monkey999por <yururi1122@gmail.com>
2020-02-16 11:20:29 +0900

    環境変数読み込み追加

--- batch/mls.bat
+++ batch/mls.bat
@@ -2,13 +2,7 @@
 pushd "%~dp0"
 rem 環境変数(主にパス系)設定
 
-for /f "delims=" %%s in (.\props\env_path.sete) do (
- %%s
- echo %%s
-)
-
-echo %start_page%
-exit /b 0
+call .\props\env_path_set.bat
 
 setlocal
 
@@ -20,16 +14,14 @@ rem command
 rem options...(for %command%)
 
 rem スタートページ
-.\help\hello_mls.bat
-
-
-
+call %start_page%
 
 rem 引数チェック
 if "%1"=="" (
  echo コマンド名を指定してください
  exit /b 1
 )
+
 
 set "command=%1"
 
---
+++ batch/props/env_path_set.bat
@@ -0,0 +1,5 @@
+rem mlsで使用される環境変数です。
+rem ほかの変数を使用する場合は、その変数が自分より上側に定義されている必要があります
+
+set current_path=C:\develop\my_script\batch
+set start_page=%current_path%\help\hello_mls.bat
