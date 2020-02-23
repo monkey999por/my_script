c2d50488b1c40e5da3dbf628712b8aa8e2758abb
monkey999por <yururi1122@gmail.com>
2020-02-24 01:54:55 +0900

    Revert "interface"
    This reverts commit aafcad90b8a90997bfa2e8de5dab6ec1cf04fa25.

--- batch/command/sample/sample.bat
+++ batch/command/sample/sample.bat
@@ -1,4 +1,19 @@
 @echo off
 setlocal
 
-rem サンプルです
+rem サンプル確認用
+
+if "%1"=="" (
+ echo 引数の確認用です
+ echo 引数をエコーします
+ exit /b 1
+)
+
+echo 全引数
+echo %*
+echo %1
+echo %2
+echo %3
+echo %4
+echo %5
+echo %6
