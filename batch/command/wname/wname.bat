45214830f3d57c87283093c9ca6e8851f46a4d46
monkey999por <yururi1122@gmail.com>
2020-02-24 23:41:45 +0900

    wname更新!

--- batch/command/wname/wname.bat
+++ batch/command/wname/wname.bat
@@ -8,13 +8,18 @@ rem あとでpathとpathextはファイル整形して読み込むようにする
 
 rem 書き出し先 default %userprofile%\Downloads\result_wname.txt
 
+rem 初期処理
+set "temp_wname=.\output\temp_wname.txt"
+set "wname=.\output\wname.txt"
+@del %temp_wname% 2>nul
+@del %wname% 2>nul
+
 if "%1"=="--help" (
  echo.
  echo 環境変数pathにあるpathextの拡張子のファイル（＝名前だけで実行可能なファイル）の一覧を作成します
  echo 出力先：%cd%\output\temp_wname.txt
  exit /b 0
 )
-
 setlocal  ENABLEDELAYEDEXPANSION
 for /f "delims=" %%f in (.\input\my_path.txt) do (
  set "my_path=%%f"
@@ -22,17 +27,14 @@ for /f "delims=" %%f in (.\input\my_path.txt) do (
   set "my_pathext=%%g"
   
   rem 検索して結果をテキストに書き出し
-  
-  @dir /b "!my_path!\*!my_pathext!" >> .\output\temp_wname.txt 2>nul
+  @dir /b "!my_path!\*!my_pathext!" >> %temp_wname% 2>nul
  )
 )
 
 rem パスが出るように整形
-for /f "delims=" %%f in (.\output\temp_wname.txt) do (
+for /f "delims=" %%f in (%temp_wname%) do (
  echo 検索中 %%f
- @where %%f >> .\output\wname.txt
+ @where %%f >> %wname%
 )
-del .\output\temp_wname.txt
-
+del %temp_wname%
 popd
-
