c07f240c3dc0e2b64bfc1e496ed017f249da3c5f
monkey999por <yururi1122@gmail.com>
2020-02-26 22:17:54 +0900

    saburu-tinn

--- batch/command/sample/sample.bat
+++ batch/command/sample/sample.bat
@@ -6,8 +6,20 @@ rem サンプル確認用
 if "%1"=="" (
  echo 引数の確認用です
  echo 引数をエコーします
+ 
+ echo --call サブルーチンのテスト
+ 
+ exit /b 1
+ 
+ 
+)
+
+if "%1"=="--call" (
+ call :Test1
+ echo 終わり
  exit /b 1
 )
+
 
 echo 全引数
 echo %*
@@ -17,3 +29,18 @@ echo %3
 echo %4
 echo %5
 echo %6
+
+
+:Test1
+echo test1
+call :Test2
+exit /b 0
+
+:Test2
+echo test2
+call :Test3
+exit /b 0
+
+:Test3
+echo test3
+exit /b 0
