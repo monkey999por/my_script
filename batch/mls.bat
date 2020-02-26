49341e4bf39d7d9dcb486c7384e2d9f672ca29b8
monkey999por <yururi1122@gmail.com>
2020-02-26 20:53:57 +0900

    Revert "temp"
    This reverts commit b40b6afc22e3015546f0f8c1efa93ae43b66cb96.

--- batch/mls.bat
+++ batch/mls.bat
@@ -23,27 +23,12 @@ if "%1"=="--help" (
  echo 実行可能コマンド一覧
  echo 詳細はcommand --helpで確認してください
  echo.
- 
- rem dirで検索出来たら出力する
- set "mls_list=%temp%mls_list.txt"
- dir /b %command_path% > %mls_list%
-
  setlocal ENABLEDELAYEDEXPANSION
  for /f "tokens=1,2 delims==" %%a in (%command_help%) do (
-  set "command_name=%%a"
-  set "description=%%b"
-  findstr /i /x /l	"!command_name!" %mls_list% > nul
-  echo エラーレベル%errorlevel%
-  if errorlevel 0 (
-   echo [!command_name!]   !description!
-   echo.
-  ) else (
-   echo やばい	
-  )
-   
+  echo [%%a]  %%b
+  echo.
  )
  endlocal
- del %mls_list%
  exit /b 0
 )
 
