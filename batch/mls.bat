795c176b943f6d8d85eb279e6d2d8eade761a26c
monkey999por <yururi1122@gmail.com>
2020-03-01 20:37:51 +0900

    testオプション追加

--- batch/mls.bat
+++ batch/mls.bat
@@ -20,12 +20,16 @@ if "%1"=="" (
 )
 
 rem help表示
-if "%1"=="--help" ( %common_help% %1 )
+if "%1"=="--help" (
+ call %common_help% %1
+ cd %this%
+)
 
 rem 開発用。指定したコマンド名でひな形を作成する
 if "%1"=="--new" (
  if "%2"=="" (
   echo コマンド名を指定してください。
+  cd %this%
   exit /b 1
  )
  
@@ -33,6 +37,7 @@ if "%1"=="--new" (
  for /f %%c in ('dir /b %command_path%') do (
   if "%2"=="%%c" (
    echo すでに同名コマンドが存在します。別の名前を指定してください。
+   endlocal & cd %this%
    exit /b 1
   )
  )
@@ -43,27 +48,48 @@ if "%1"=="--new" (
  
  rem フォルダを作成する
  mkdir %command_path%\%2
- if not errorlevel 0 ( echo 異常終了しました & exit /b 1 )
  
  rem ファイルを配置する
  copy %current_path%\ひな形.bat %command_path%\%2\%2.bat
- if not errorlevel 0 ( echo 異常終了しました & exit /b 1 )
  
  copy %current_path%\ひな形.bat %command_path%\%2\%2_test.bat
- if not errorlevel 0 ( echo 異常終了しました & exit /b 1 )
  
  echo call %command_path%\%2\%2.bat >> %command_path%\%2\%2_test.bat
- if not errorlevel 0 ( echo 異常終了しました & exit /b 1 )
  
  rem command.helpにhelpを追加する
  rem 内容は"command.helpに説明が書いてありません"
  echo %2=command.helpに説明が書いてありません >> %command_help%
- if not errorlevel 0 ( echo 異常終了しました & exit /b 1 )
- 
  
  echo 正常終了しました
+ cd %this%
  exit /b 0
 )
+
+rem カスタムコマンドのテスト実行用
+if "%1"=="--test" (
+ if "%2"=="" (
+  echo コマンド名を指定してください。
+  cd %this%
+  exit /b 1
+ )
+ 
+ setlocal ENABLEDELAYEDEXPANSION
+ for /f %%c in ('dir /b %command_path%') do (
+  if "%2"=="%%c" (
+   call %command_path%\%2\%2_test.bat
+   endlocal & cd %this%
+   exit /b 1
+  ) 
+ )
+ endlocal
+ cd %this%
+ exit /b 0
+)
+
+
+rem ---------------------------------------
+rem ここからカスタムコマンド実行の本処理です
+rem ---------------------------------------
 
 setlocal
 rem コマンド存在チェック
@@ -87,7 +113,7 @@ exit /b 1
 :Run
 rem カスタムコマンド実行
 rem 引数を一度ファイルに書き出して、それをforステートメントに渡す
-rem ⇒こうしないとfor分の引数の関係でダブルクォーテーション付き引数がうまく認識されない
+rem ⇒こうしないとfor文の引数の関係でダブルクォーテーション付き引数がうまく認識されない
 del %temp%\mls_run.txt
 echo %* > %temp%\mls_run.txt
 
