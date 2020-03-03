3ffee6897204da135eb66e86ed73d2de5fd6c2ad
monkey999por <yururi1122@gmail.com>
2020-03-04 00:39:42 +0900

    mls_cmd

--- My_Local_Script/mls_props/env_path_set.bat
+++ My_Local_Script/mls_props/env_path_set.bat
@@ -3,12 +3,18 @@ rem ほかの変数を使用する場合は、その変数が自分より上側に定義されている必要があり
 
 rem ディレクトリ構成です。ディレクトリ構成を変更する場合はここを変えれば良いはず
 set current_path=C:\develop\my_script\My_Local_Script
-set mls_batch=%current_path%\mls_batch
-set mls_internal_batch=%current_path%\mls_internal_batch
+rem current_path直下
 set mls_props=%current_path%\mls_props
 set mls_temp=%current_path%\mls_temp
 
-set command_help=%current_path%\props\command.help
+
+set mls_cmd=%current_path%\mls_cmd
+rem mls_cmd直下
+set mls_batch=%mls_cmd%\mls_batch
+set mls_internal_batch=%mls_cmd%\mls_internal_batch
+
+
+set command_help=%mls_props%\command.help
 
 rem hello ページ
 set start_page=%mls_batch%\hello_mls\hello_mls.bat
