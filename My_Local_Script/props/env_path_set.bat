316e81932320b1481020ac153132026664cb7445
monkey999por <yururi1122@gmail.com>
2020-03-04 00:11:51 +0900

    internal_command -> mls_internal_comamnd

--- My_Local_Script/props/env_path_set.bat
+++ My_Local_Script/props/env_path_set.bat
@@ -4,20 +4,16 @@ rem ほかの変数を使用する場合は、その変数が自分より上側に定義されている必要があり
 rem ディレクトリ構成です。ディレクトリ構成を変更する場合はここを変えれば良いはず
 set current_path=C:\develop\my_script\My_Local_Script
 set mls_batch=%current_path%\mls_batch
+set mls_internal_batch=%current_path%\mls_internal_batch
 set mls_temp=%current_path%\mls_temp
-
-
-
-
-set internal_path=%current_path%\internal_command
 
 set command_help=%current_path%\props\command.help
 
 rem hello ページ
-set start_page=%current_path%\command\hello_mls\hello_mls.bat
+set start_page=%mls_batch%\hello_mls\hello_mls.bat
 
 rem 共通エラーハンドリング
-set "error_h=%internal_path%\error\error_handling.bat"
+set "error_h=%mls_internal_batch%\error\error_handling.bat"
 
 rem 共通help出力
-set "common_help=%internal_path%\help\common_help.bat"
+set "common_help=%mls_internal_batch%\help\common_help.bat"
